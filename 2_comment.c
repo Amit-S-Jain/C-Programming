8053e90069294dbfe954ab0ed0840a15c17400d8
Amit14341 <54832649+Amit14341@users.noreply.github.com>
2020-07-26 18:23:39 +0530

    Comments in C programming

---
+++ 2_comment.c
@@ -0,0 +1,14 @@
+//Coments :- Coments are the statements which doesn't executed at the time of code execution. These statements are skipped by the terminal
+//Comments are used to understand the code. With the help of comment, other developers are also able to contribute or understand the code done by another developer.
+//Comments are of two type, Single line comment and Multiline comment.
+
+//In C programming '//' this symbol is used to give a single line comment and for multiline comment we can use '/*' to start a multiline comment and '*/' to end a multiline comment
+
+
+// Single Line Comment
+
+/*
+Multi
+Line
+Comment
+*/
